77b9285ecd8045e192283815bfabd2886ce3ce49
justinscandale <100536879+justinscandale@users.noreply.github.com>
2025-01-05 16:03:44 -0500

    Created routes / began error handling

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("server");
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server Started on Port ${port}`))
+
