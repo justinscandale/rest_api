1beb7593d48d57dc107ac5514467018047429ec2
justinscandale <100536879+justinscandale@users.noreply.github.com>
2025-01-16 01:02:59 -0500

    Added features to user and goal

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector} from 'react-redux';
+import GoalForm from "../components/GoalForm";
+
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const {user} = useSelector((state)=>state.auth)
+  
+  useEffect(() => {
+    if(!user) {
+      navigate('login')
+    }
+  }, [user, navigate]);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <>
+    <section className='heading'>
+      <h1> Welcome {user && user.name} </h1>
+      <p>goals dashboard</p>
+    </section>
+    <GoalForm />
+    </>
   );
 }
 
